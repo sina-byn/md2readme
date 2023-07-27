ac500b5ebe2582434f43df929776977e89626274
sina-byn <sina.sina.bayan@protonmail.com>
2023-07-27 21:38:04 +0330

    header and main container added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+// * components
+import Container from './components/Container';
+
 const App = () => {
   return (
-    <div className="font-">
-      readme2md
+    <div className='app-container'>
+      <header className='app-header h-[65px] bg-zinc-800 text-gray-200 px-2 md:px-5'>
+        <Container className='flex justify-center items-center'>
+          <h1 className='app-title text-xl font-bold'>md2readme</h1>
+        </Container>
+      </header>
+      <main className='main-container w-screen h-[calc(100vh_-_65px)] p-2 md:p-5'>
+        <Container className='grid lg:grid-cols-12 gap-x-5'>
+          {/* main content goes here */}
+        </Container>
+      </main>
     </div>
   );
 };
