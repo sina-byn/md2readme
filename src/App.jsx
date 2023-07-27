9c39742884a139ced80022f8d7c884df7f07ae5e
sina-byn <sina.sina.bayan@protonmail.com>
2023-07-27 21:29:54 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="font-">
+      readme2md
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'vs-dark': '#1e1e1e',
+      },
+    },
+  },
+  plugins: [],
+};
